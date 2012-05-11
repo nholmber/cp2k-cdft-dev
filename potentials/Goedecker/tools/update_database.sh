8798740107c5d6fa59b779d0ef4be53fa28f35de
Matthias Krack <mkrack@cp2k.org>
2012-05-11 07:48:55 +0000

    Automatic compilation of the format converter
    git-svn-id: svn://svn.code.sf.net/p/cp2k/code/trunk@12223 bac0169d-1eea-4375-b5f6-f89d2ced9994

--- potentials/Goedecker/tools/update_database.sh
+++ potentials/Goedecker/tools/update_database.sh
@@ -7,6 +7,9 @@
 
 # Generate the CP2K and ABINIT files by converting the
 # psp.par files in the build/ tree
+  if [[ ! -f gth_pp_convert.x ]]; then
+    g95 -o gth_pp_convert.x gth_pp_convert.f90
+  fi
   ./gth_pp_convert.sh
 
 # Copy all XX files to the database in ../cpmd
